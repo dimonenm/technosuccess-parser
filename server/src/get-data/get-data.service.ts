d5ab588499d3dc5d88fd16fa817fdb7ee882fa8b
exp <arzuakov@yahoo.com>
2025-04-08 00:49:51 +0300

    08.04.2025

--- server/src/get-data/get-data.service.ts
+++ server/src/get-data/get-data.service.ts
@@ -10,19 +10,28 @@ export class GetDataService {
         search_performed: 'Y',
         q: '16gb perfeo',
         dispatch: 'products.search',
-        page: '2'
+        page: '2',
       },
       headers: {
         cookie: 'sid_customer_733c5=1b089ec0915b3f3a39e8d595e231def0-1-C',
       },
     };
-    
+
     const res = await axios.get('https://technosuccess.ru', config);
 
-
-
     const root = parse(res.data);
-    console.log(root.querySelector('.pagination')?.querySelectorAll('li').length);
-    return res.data;
+    console.log(
+      root.querySelector('.pagination')?.querySelectorAll('li').length,
+    );
+    
+    root.querySelectorAll('form.category-list-item').forEach((item) => {
+      console.log(item.querySelector('.product-title')?.innerText);
+      console.log(item.querySelector('.ty-previewer')?.attrs.href);
+      console.log(item.querySelector('.ty-price-num')?.innerText);
+    });
+
+    const list = root.querySelectorAll('form.category-list-item');
+    return list?.toString();
+    // return res.data;
   }
 }
