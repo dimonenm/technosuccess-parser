a281be791169d0badce67697e7cd89e4059f4353
Dmitriy_Arzyakov <arzuakov@yahoo.com>
2025-04-18 17:32:59 +0300

    18.04.2025

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Roboto", "sans-serif"],
+      },
+    },
   },
   darkMode: "class",
   plugins: [heroui()],
