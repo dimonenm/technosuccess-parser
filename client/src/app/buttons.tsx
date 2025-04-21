cf4af1007d969064e34f7539f1bbb2e5657ff14c
Dmitriy_Arzyakov <arzuakov@yahoo.com>
2025-04-21 17:43:59 +0300

    add inputs

--- client/src/app/buttons.tsx
+++ client/src/app/buttons.tsx
@@ -1,15 +1,21 @@
-"use client"
-import { Button } from '@heroui/react'
+'use client'
+import { Button, Input } from '@heroui/react'
 
 export default function Buttons() {
   return (
-    <div className='flex flex-wrap gap-4 items-center'>
-      <Button color='default'>Default</Button>
-      <Button color='primary'>Primary</Button>
-      <Button color='secondary'>Secondary</Button>
-      <Button color='success'>Success</Button>
-      <Button color='warning'>Warning</Button>
-      <Button color='danger'>Danger</Button>
-    </div>
+    <>
+      <div className='flex flex-wrap gap-4 items-center'>
+        <Button color='default'>Default</Button>
+        <Button color='primary'>Primary</Button>
+        <Button color='secondary'>Secondary</Button>
+        <Button color='success'>Success</Button>
+        <Button color='warning'>Warning</Button>
+        <Button color='danger'>Danger</Button>
+      </div>
+      <div className='flex w-full flex-wrap md:flex-nowrap gap-4'>
+        <Input label='Email' type='email' />
+        <Input label='Email' placeholder='Enter your email' type='email' />
+      </div>
+    </>
   )
 }
