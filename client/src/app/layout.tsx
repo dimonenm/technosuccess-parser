d2968d507a42961c001624fdd8439670cf5a46d6
exp <arzuakov@yahoo.com>
2025-04-24 02:25:36 +0300

    24.04.2025

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body>
+      <body className='container '>
         <Providers>{children}</Providers>
       </body>
     </html>
