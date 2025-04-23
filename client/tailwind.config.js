a29fda098b1178eaf441256ae23cd020950678a6
exp <arzuakov@yahoo.com>
2025-04-24 02:39:29 +0300

    24.04.2025

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,8 @@
-import { heroui } from "@heroui/react";
+import { heroui } from "@heroui/react"
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./client/src/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
